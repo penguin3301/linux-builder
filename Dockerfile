400f914839879b749ce725b8daae478659b7ca9e
penguin3301 <119744518+penguin3301@users.noreply.github.com>
2023-01-28 21:36:29 +0000

    ccache

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM archlinux:latest
 
-RUN pacman -Syyu --noconfirm --needed git base-devel bc cpio github-cli asp shadow  bc libelf pahole cpio perl tar xz gettext xmlto python-sphinx python-sphinx_rtd_theme graphviz imagemagick texlive-latexextra git
+RUN pacman -Syyu --noconfirm --needed ccache git base-devel bc cpio github-cli asp shadow  bc libelf pahole cpio perl tar xz gettext xmlto python-sphinx python-sphinx_rtd_theme graphviz imagemagick texlive-latexextra git
 
 RUN useradd -m -G wheel builder
 RUN echo "%wheel ALL=(ALL:ALL) NOPASSWD:ALL" >/etc/sudoers.d/00-sudo-nopasswd
 
 RUN mkdir /data
-VOLUME /data
 
 USER builder
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,15 @@ set -e
 repo="$(printenv REPO)"
 
 cd "/data"
+
 sudo mkdir "build"
 sudo chown -vR "builder:builder" "build"
-sudo chmod -v 700 build
-ls -l
+sudo chmod -v 700 "build"
+
+sudo mkdir "cache"
+sudo chown -vR "builder:builder" "cache"
+sudo chmod -v 700 "cache"
+
 cd "build"
 
 gpg --keyserver keys.openpgp.org --recv-keys 19802F8B0D70FC30
@@ -21,6 +26,12 @@ cd linux
 
 sed -i 's/^pkgbase=.*$/pkgbase=linux-alexcoder04/' PKGBUILD
 sed -i 's/^pkgname=.*/pkgname=("$pkgbase" "$pkgbase-headers")/' PKGBUILD
+
+export CCACHE_DIR="/data/cache"
+export CC="ccache gcc"
+export MAKEFLAGS="-j$(nproc)"
+ccache -M 9
+ccache -s
 
 makepkg -s
 
