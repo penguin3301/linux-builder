1cbc97998bfed48781f5003bcf39039370f0f9b7
penguin3301 <119744518+penguin3301@users.noreply.github.com>
2023-01-28 17:21:06 +0000

    edit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM archlinux:latest
 RUN pacman -Suy --noconfirm --needed git base-devel bc cpio github-cli
-RUN useradd -m xanmod_builder
-USER xanmod_builder
+USER builder
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,19 @@
-#!/bin/bash
-commit_hash="9017d5010ce20617d5fa700cba206c434875e1be"
-repo=`printenv REPO`
-cd /home/xanmod_builder
-git clone https://aur.archlinux.org/linux-xanmod.git &&
-cd linux-xanmod
-echo "Checkout specified commit..."
-git checkout $commit_hash &&
-echo "Compiling kernel..."
-env MAKEFLAGS="-s -j$(nproc)" _microarchitecture=34 use_numa=n use_tracers=n makepkg --skippgpcheck &&
-echo "Logining in to GitHub..."
-printenv GITHUB_KEY | gh auth login --with-token
-version=`git log --format=%B -n 1 $commit_hash | awk -F '-' 'NR==1{print "v"$1}'`
-gh release view "$version" --repo "$repo"
-tag_exists=$?
-if test $tag_exists -eq 0; then
-    echo "Tag already exists!"
-    echo "Removing previous release..."
-    gh release delete "$version" -y --cleanup-tag --repo "$repo"
-else
-    echo "Tag does not exist!"
-fi
-echo "Releasing $version binaries into $repo"
-gh release create "$version" ./*.pkg.tar.zst --repo "$repo" &&
-echo "Released!"
-echo "Loging out from Github..."
+#!/bin/sh
+
+repo="$(printenv REPO)"
+
+mkdir -v "$HOME/build"
+cd "$HOME"
+
+asp update linux
+asp export linux
+
+cd linux
+
+sed -i 's/^pkgbase=.*$/pkgbase=linux-alexcoder04/' PKGBUILD
+sed -i 's/^pkgname=.*/pkgname=("$pkgbase" "$pkgbase-headers")/' PKGBUILD
+
+makepkg -s
+
+gh release create "latest" ./*.pkg.tar.zst --repo "$repo"
 gh auth logout -h github.com
